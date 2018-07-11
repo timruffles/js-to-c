3f1a4c8bd0a316d7eb9241b573c6d2e2a440042b
Tim Ruffles <timruffles@gmail.com>
2018-07-11 11:02:33 +0100

    Hello world with typescript

---
+++ src/js-to-c.ts
@@ -0,0 +1,119 @@
+import fs from 'fs';
+import {parseScript, Syntax} from "esprima";
+import {
+    Node, Program
+} from 'estree';
+
+
+type NodeCompiler = (n: any) => string;
+
+type NodeCompilerLookup = {
+    [k in keyof typeof Syntax]: NodeCompiler
+}
+
+const lookup = getCompilers();
+
+if(require.main === module) {
+    main();
+}
+
+function main() {
+    const input = fs.readFileSync(process.argv[2], { encoding: 'utf8'});
+
+    const ast = parseScript(input);
+
+    console.log(compile(ast));
+}
+
+function compile(ast: Node): string {
+    return lookup[ast.type](ast);
+}
+
+function getCompilers(): NodeCompilerLookup {
+    return {
+        ArrayExpression: unimplemented('ArrayExpression'),
+        ArrayPattern: unimplemented('ArrayPattern'),
+        ArrowFunctionExpression: unimplemented('ArrowFunctionExpression'),
+        AssignmentExpression: unimplemented('AssignmentExpression'),
+        AssignmentPattern: unimplemented('AssignmentPattern'),
+        AwaitExpression: unimplemented('AwaitExpression'),
+        BinaryExpression: unimplemented('BinaryExpression'),
+        BlockStatement: unimplemented('BlockStatement'),
+        BreakStatement: unimplemented('BreakStatement'),
+        CallExpression: unimplemented('CallExpression'),
+        CatchClause: unimplemented('CatchClause'),
+        ClassBody: unimplemented('ClassBody'),
+        ClassDeclaration: unimplemented('ClassDeclaration'),
+        ClassExpression: unimplemented('ClassExpression'),
+        ConditionalExpression: unimplemented('ConditionalExpression'),
+        ContinueStatement: unimplemented('ContinueStatement'),
+        DebuggerStatement: unimplemented('DebuggerStatement'),
+        DoWhileStatement: unimplemented('DoWhileStatement'),
+        EmptyStatement: unimplemented('EmptyStatement'),
+        ExportAllDeclaration: unimplemented('ExportAllDeclaration'),
+        ExportDefaultDeclaration: unimplemented('ExportDefaultDeclaration'),
+        ExportNamedDeclaration: unimplemented('ExportNamedDeclaration'),
+        ExportSpecifier: unimplemented('ExportSpecifier'),
+        ExpressionStatement: unimplemented('ExpressionStatement'),
+        ForInStatement: unimplemented('ForInStatement'),
+        ForOfStatement: unimplemented('ForOfStatement'),
+        ForStatement: unimplemented('ForStatement'),
+        FunctionDeclaration: unimplemented('FunctionDeclaration'),
+        FunctionExpression: unimplemented('FunctionExpression'),
+        Identifier: unimplemented('Identifier'),
+        IfStatement: unimplemented('IfStatement'),
+        Import: unimplemented('Import'),
+        ImportDeclaration: unimplemented('ImportDeclaration'),
+        ImportDefaultSpecifier: unimplemented('ImportDefaultSpecifier'),
+        ImportNamespaceSpecifier: unimplemented('ImportNamespaceSpecifier'),
+        ImportSpecifier: unimplemented('ImportSpecifier'),
+        LabeledStatement: unimplemented('LabeledStatement'),
+        Literal: unimplemented('Literal'),
+        LogicalExpression: unimplemented('LogicalExpression'),
+        MemberExpression: unimplemented('MemberExpression'),
+        MetaProperty: unimplemented('MetaProperty'),
+        MethodDefinition: unimplemented('MethodDefinition'),
+        NewExpression: unimplemented('NewExpression'),
+        ObjectExpression: unimplemented('ObjectExpression'),
+        ObjectPattern: unimplemented('ObjectPattern'),
+        Program,
+        Property: unimplemented('Property'),
+        RestElement: unimplemented('RestElement'),
+        ReturnStatement: unimplemented('ReturnStatement'),
+        SequenceExpression: unimplemented('SequenceExpression'),
+        SpreadElement: unimplemented('SpreadElement'),
+        Super: unimplemented('Super'),
+        SwitchCase: unimplemented('SwitchCase'),
+        SwitchStatement: unimplemented('SwitchStatement'),
+        TaggedTemplateExpression: unimplemented('TaggedTemplateExpression'),
+        TemplateElement: unimplemented('TemplateElement'),
+        TemplateLiteral: unimplemented('TemplateLiteral'),
+        ThisExpression: unimplemented('ThisExpression'),
+        ThrowStatement: unimplemented('ThrowStatement'),
+        TryStatement: unimplemented('TryStatement'),
+        UnaryExpression: unimplemented('UnaryExpression'),
+        UpdateExpression: unimplemented('UpdateExpression'),
+        VariableDeclaration: unimplemented('VariableDeclaration'),
+        VariableDeclarator: unimplemented('VariableDeclarator'),
+        WhileStatement: unimplemented('WhileStatement'),
+        WithStatement: unimplemented('WithStatement'),
+        YieldExpression: unimplemented('YieldExpression')
+    }
+}
+
+function Program() {
+    return `
+        #include <stdio.h>
+        
+        int main(int argc, char**argv) {
+            printf("Hello world");
+            return 0;
+        }
+    `
+}
+
+function unimplemented(node: string) {
+    return function () {
+        return `/* unimplemented ${node} */`;
+    }
+}
