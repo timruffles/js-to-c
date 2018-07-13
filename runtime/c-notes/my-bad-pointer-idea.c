09a65fb16038080ab4b8e46ee189a2fc02cfc789
Tim Ruffles <timruffles@gmail.com>
2018-07-13 08:53:38 +0100

    messing around with ptrs

--- runtime/c-notes/my-bad-pointer-idea.c
+++ runtime/c-notes/my-bad-pointer-idea.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdint.h>
+#include <string.h>
 
 /**
- * So, plan was to store the values for the JS compiler
+ * So, plan is to store the values for the JS compiler
  * in a union, nicking Lua's idea. That way immediate
  * values like numbers live inside the JSValue wrapper,
- * while the compound values lived elsewhere:
+ * while the compound values lived elsewhere.
  */
 
 typedef char* TypeTag;
@@ -22,13 +23,27 @@ typedef union {
     void* object;
     double number;
     BooleanValue boolean;
+
+    uintptr_t raw;
 } JsValueContent;
 
 typedef struct {
-    // lets us discriminate the union
+    // to discriminate the union
     TypeTag type;
     JsValueContent value;
 } JsValue;
+
+// some inspection
+void printBytes(void*, uint64_t);
+void printBytes(void* addressOfSomething, uint64_t bytes) {
+    char* charPtr = addressOfSomething;
+    printf("Starting at %p, bytes: ", (void*)charPtr);
+    for(uint64_t i = 0; i < bytes; i++) {
+        int8_t byte = *(charPtr + i);
+        printf("%#x ", byte);
+    }
+    printf("\n");
+} 
 
 
 /**
@@ -39,67 +54,132 @@ typedef struct {
  * makes more sense - but I'm interested in that it wasn't doable.
  *
  * My first though was to both pass in and return the union
- * as a uintptr_t - as it was going to be as wide as a single
- * pointer:
+ * as a uintptr_t - as it was going to be as wide as a 
+ * pointer.
+ *
+ * I noticed two things:
+ * - this makes for a LOT of ugly casting to make the compiler
+ *   stop pointing out it's a bad idea
+ * - it's easy to break things if you do casts that will cause
+ *   data conversion (especially double -> uintptr_t) rather than
+ *   simply a view of a different type of the same data
  */
 
 
 typedef uintptr_t OpaqueValue;
 
-JsValue* jsValueCreate(TypeTag, OpaqueValue);
-JsValue* jsValueCreate(TypeTag tag, OpaqueValue value) {
+JsValue* jsValueCreate(TypeTag, uintptr_t);
+JsValue* jsValueCreate(TypeTag tag, uintptr_t value) {
     JsValue* val = calloc(sizeof(JsValue), 1);
     *val = (JsValue) {
         .type = tag,
     };
-    val->value.object = (void*)value;
+    // Although the union elements are of different sizes
+    // unions' elements are all aligned to address of union,
+    // so we can just read the max number of bytes for every
+    // argument.
+    //
+    // Argument that this is ok: char is well defined as a single
+    // byte, so having 7 bytes of garbage after is not a problem.
+    //
+    // Pointers and doubles both take 8 bytes, so we're never
+    // getting any garbage anyway.
+    //
+    //     jsValueCreate(.., 'Y')
+    //
+    //     jsValueCreate(.., 47.0)
+    //
+    //     jsValueCreate(.., somePtr)
+    //
+    //memcpy(&val->value, &value, sizeof(JsValueContent));
+    val->value.raw = value;
     return val;
 }
 
-OpaqueValue jsValueGet(JsValue*);
-OpaqueValue jsValueGet(JsValue* value) {
-    return (OpaqueValue) value->value.object;
+uintptr_t jsValueGet(JsValue*);
+uintptr_t jsValueGet(JsValue* value) {
+    printBytes(&value->value, sizeof(JsValueContent));
+    return (uintptr_t)value->value.object;
 }
 
-void printBytes(char* addressOfSomething, uint64_t bytes) {
-    printf("Starting at %#x, bytes: ", addressOfSomething);
-    for(uint64_t i = 0; i < bytes; i++) {
-        uint8_t byte = *(addressOfSomething + i);
-        printf("%#x ", byte);
-    }
-    printf("\n");
-} 
-
-
-
-int main() {
+void manuallyMessingAround(void);
+void manuallyMessingAround() {
+    // check our assumptions - the union is as wide as a ptr value
     assert(sizeof(JsValueContent) == sizeof(void *));
 
-    printf("Ok - we're going to store the string 'lols', then print its bytes\n");
+    printf("\nPart One\n\n");
+    printf("Ok - we're going to store the double 4242, then print its bytes\n");
     double pretendObject = 4242;
     printBytes(&pretendObject, sizeof(void *));
-    printf("Next we're going to store it in a union, and do the same\n");
+
+    printf("\nNext we're going to store it in a union, and do the same\n");
     JsValueContent content = { .number = pretendObject };
     printBytes(&content, sizeof(void *));
 
-    printf("Then, take the union and pop it into a uintptr_t");
-    char* addressOfUnion = &content;
-    uintptr_t data = addressOfUnion;
-    printBytes(data, sizeof(void *));
+    printf("\nThen, take the union and pop it into a uintptr_t\n");
+    uintptr_t* ptr = (void *)(&content);
+    uintptr_t got_it_a = *ptr;
 
-    printf("The, take that uintptr_t and turn it into a double\n");
-    char* asWhatever = &data;
-    double doubleAgain = *asWhatever;
+    // this isn't safe - as it reverses the direction with the cast
+    // uintptr_t one_liner = (uintptr_t)(void *)(&content);
+
+    // squash the original variable just to make it ultra clear
+    // we have got the same bytes in got_it as we had in the location
+    // of content
+    uintptr_t got_it = got_it_a;
+    got_it_a = 7;
+
+    printBytes(&got_it, sizeof(void *));
+
+    printf("\nThen, take that uintptr_t and turn it into a double\n");
+    void* asWhatever = &got_it;
+    double* doublePtr = asWhatever;
+    double doubleAgain = *doublePtr;
     printBytes(&doubleAgain, sizeof(void *));
 
+    printf("\nFinally, print the location of all of the copies of our data, and their values\n\n");
 
-    JsValue* val1 = jsValueCreate(NUMBER, 42.0);
+    printf("pretendObject - ");
+    printBytes(&pretendObject, sizeof(void *));
+
+    printf("content - ");
+    printBytes(&content, sizeof(void *));
+
+    printf("got_it - ");
+    printBytes(&got_it, sizeof(void *));
+
+    printf("doubleAgain - ");
+    printBytes(&doubleAgain, sizeof(void *));
+}
+
+void jsValueTests(void);
+void jsValueTests() {
+    double pretendObject = 4242;
+    printf("\n\n\nPart Two\n\n");
+    double doubleVal = 47;
+    JsValue* val1 = jsValueCreate(NUMBER, *(uintptr_t*)(void*)&doubleVal);
     JsValue* val2 = jsValueCreate(BOOLEAN, TRUE);
     JsValue* val3 = jsValueCreate(OBJECT, &pretendObject);
 
-    printf("val1 %f - \n", jsValueGet(val1)); 
-    printf("val2 %c\n", (char)jsValueGet(val2)); 
+    uintptr_t r = jsValueGet(val1);
 
-    int* restoredPointer = (int*)jsValueGet(val3);
-    printf("*val3 %i\n", *restoredPointer); 
+    // copy out our double value into another double variable
+    double dblRestored = *(double*)(void*)&r;
+    assert(dblRestored - doubleVal < 0.0000000001);
+    printf("val1 %f - \n", dblRestored);
+
+    // read out the char
+    char readChar = (char)jsValueGet(val2);
+    assert(readChar == TRUE);
+    printf("val2 %c\n", readChar); 
+
+    // read out the pointer value
+    void* restoredPointer = jsValueGet(val3);
+    assert(restoredPointer == &pretendObject);
+    printf("*val3 %p\n", restoredPointer); 
+}
+
+int main() {
+    manuallyMessingAround();
+    jsValueTests();
 }
