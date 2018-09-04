229e37a07401fe41425675fe7b844f78dd1c2757
Tim Ruffles <timruffles@gmail.com>
2018-09-04 17:37:07 +0100

    Clean up debug

--- runtime/lib/debug.h
+++ runtime/lib/debug.h
@@ -16,29 +16,19 @@
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d:%s) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d:%s) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
 #define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d:%s:) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
-#define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+#define ensure(T, M) if(!(T)) { log_err(#T); exit(1); }
 
-#define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
-
-#define check_mem(A) check((A), "Out of memory.")
-
-#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; }
-
-#define todo(M) log_err(M); goto fail;
-
-#define ensure(T) if(!(T)) { log_err(#T); exit(1); }
+#define precondition(T, M, ...) if(!(T)) { log_err(M, ##__VA_ARGS__); exit(1); }
 
 #define ensureAlloced(V) if((V) == NULL) { log_err("Could not allocate memory for '%s'", #V); exit(1); }
 
 #define fail(M, ...)  { log_err(M, ##__VA_ARGS__); exit(1); }
-
-
 
 
 #pragma clang diagnostic pop
