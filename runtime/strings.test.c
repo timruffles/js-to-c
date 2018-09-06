5c5dab9cda2408109820bd62c58c2b584aa2b5dd
Tim Ruffles <timruffles@gmail.com>
2018-09-06 14:15:12 +0100

    normalise macro case

--- runtime/strings.test.c
+++ runtime/strings.test.c
@@ -8,38 +8,43 @@
 #include "test.h"
 #include "gc.h"
 
-#define INTERNED_STRING(S) stringCreateFromInternedString(S, strlen(S))
+#define internedString(S) stringCreateFromInternedString(S, strlen(S))
 #define assertCStringValue(S,CS) assert(strcmp(stringGetCString(S), CS) == 0);
 
 static void itCanCreateAStringFromACString() {
-    JsValue* str = INTERNED_STRING("Hello");
+    JsValue* str = internedString("Hello");
     assert(str != NULL);
 }
 
 static void itGetsJsStringFromValue() {
     char* theContent = "Hello";
-    JsValue* str = INTERNED_STRING("Hello");
+    JsValue* str = internedString("Hello");
     assert(strcmp(stringGetCString(str), theContent) == 0);
 }
 
 static void itGetsStringLength() {
     char* theContent = "Hello";
-    JsValue* str = INTERNED_STRING("Hello");
+    JsValue* str = internedString("Hello");
     assert(strcmp(stringGetCString(str), theContent) == 0);
     assert(stringLength(str) == 5);
 }
 
 static void itCreatesStringsFromTemplate() {
     JsValue* str = stringCreateFromTemplate("Hello '%s'", "Javascript");
-    assert(stringComparison(str, INTERNED_STRING("Hello 'Javascript'")));
+    assert(stringComparison(str, internedString("Hello 'Javascript'")));
       
 }
 
 static void itJoinsStrings() {
-    JsValue* joined = stringConcat(INTERNED_STRING("One"), INTERNED_STRING("Two"));
+    JsValue* joined = stringConcat(internedString("One"), internedString("Two"));
     assertCStringValue(joined, "OneTwo");
-    assert(stringComparison(joined, INTERNED_STRING("OneTwo")));
+    assert(stringComparison(joined, internedString("OneTwo")));
     assert(stringLength(joined) == 6);
+}
+
+static void itJoinsInternedAndDynamicStrings() {
+    JsValue* joined = stringConcat(internedString("One"), stringCreateFromTemplate("%s", "TwoP"));
+    assertCStringValue(joined, "OneTwoP");
 }
 
 int main() {
@@ -50,5 +55,6 @@ int main() {
     test(itGetsStringLength);
     test(itCreatesStringsFromTemplate);
     test(itJoinsStrings);
+    test(itJoinsInternedAndDynamicStrings);
 }
 
