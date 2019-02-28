336cd0f4f2b77720e1acb54bca696d9a0db5fc63
Tim Ruffles <timruffles@gmail.com>
2019-02-28 06:34:02 -0600

    wip

--- src/operations.ts
+++ src/operations.ts
@@ -1,5 +1,6 @@
 import {Node} from "estree";
 import {CompileTimeState} from "./CompileTimeState";
+import {CompilerIdentifier, PredefinedVariableTarget} from "./js-to-c";
 
 enum HintType {
     Number = 'Number',
@@ -39,30 +40,49 @@ function compileTimeEvaluate(node: Node, state: CompileTimeState) {
     }
 }
 
-
-    /*
-    Let lref be the result of evaluating AdditiveExpression.
-    Let lval be GetValue(lref).
-    Let rref be the result of evaluating MultiplicativeExpression.
-    Let rval be GetValue(rref).
-    Let lprim be ToPrimitive(lval).
-    Let rprim be ToPrimitive(rval).
-    If Type(lprim) is String or Type(rprim) is String, then
-    Return the String that is the result of concatenating ToString(lprim) followed by ToString(rprim)
-    Return the result of applying the addition operation to ToNumber(lprim) and ToNumber(rprim). See the Note below 11.6.3.
-     */
-function addOperation(left: Node, right: Node, state: CompileTimeState) {
-
-    const lref = compileTimeEvaluate(left, state);
-    const lval =
-
+interface CompileTypeNode {
+    toSource(state: CompileTimeState): string
 }
 
-function toPrimitive(node: Node, hint: HintType, state: CompileTimeState) {
+class CFunctionCall implements CompileTypeNode {
+    readonly args: CompileTypeNode[]
+
+    constructor(
+        readonly target: CompilerIdentifier,
+        ...args: CompileTypeNode[]
+    ) {
+        this.args = args
+    }
+
+    toSource(state: CompileTimeState) {
+        const ids = this.args.map(() => new PredefinedVariableTarget('arg'))
+        return `
+            ${ids.map(id => `JsValue* ${id.id};`).join('\n')}
+            ${this.args.map(n => n.toSource(state)).join('\n')}
+            ${this.target.id}(${ids.map(id => id.id).join(', ')}
+        `
+    }
+}
+
+/*
+Let lval be GetValue(lref).
+Let rval be GetValue(rref).
+Let lprim be ToPrimitive(lval).
+Let rprim be ToPrimitive(rval).
+ */
+function addOperation(left: Node, right: Node): CompileTypeNode {
+    // if we are adding two literal number, do `jsValueNumber() + literal value`
+    // if we are adding anything to a literal string, do `strcat(stringsGetCString(), "literal val")`
+
+    //return new CFunctionCall(new PredefinedVariableTarget('addOperation'), toPrimitive(left, HintType.None), toPrimitive(right, HintType.None));
+}
+
+function toPrimitive(node: Node, hint: HintType): CompileTypeNode {
     switch(node.type) {
         case 'Literal':
             switch(typeof node.value) {
                 case 'number':
+
 
             }
     }
